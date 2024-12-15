c715ee0523779eccbecfd5742f44e1e3fdd7cc16
grym333 <afreeslave3000@gmail.com>
2024-12-15 15:24:22 -0700

    b

--- In The Zone/obj/Debug/net9.0/In The Zone.AssemblyInfo.cs
+++ In The Zone/obj/Debug/net9.0/In The Zone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("In The Zone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6da4f87ab94cbc5fdc8e167edcae81c93041cb41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+406738a9daa82b30c210eab214eca62791e7491a")]
 [assembly: System.Reflection.AssemblyProductAttribute("In The Zone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("In The Zone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
